dd7408f3428e10f75546fd8a0b8120ae599d4b66
gmunilla14 <gregmunilla@gmail.com>
2022-03-22 20:33:19 -0400

    Add test for editing plan in database

--- __tests__/Plan.spec.js
+++ __tests__/Plan.spec.js
@@ -1241,4 +1241,77 @@ describe("Edit plan", () => {
     expect(response.status).toBe(400);
     expect(response.body.message).toBe("Invalid plan input");
   });
+
+  it("edits plan in database", async () => {
+    const bicep = await Muscle.findOne({ name: "Bicep" });
+    const tricep = await Muscle.findOne({ name: "Tricep" });
+
+    const userToken1 = await createUser();
+    const userList1 = await User.find();
+    const savedUser1 = userList1[0];
+
+    //Activate User 1
+    await activateUser(userToken1, savedUser1.activationToken);
+
+    //Create User 1 exercise
+    await createExercise("Curls", [bicep._id], "notes", userToken1);
+    await createExercise("Tricep Pushdowns", [tricep._id], "notes", userToken1);
+
+    const exercise1 = await Exercise.findOne({ name: "Curls" });
+    const exercise2 = await Exercise.findOne({ name: "Tricep Pushdowns" });
+
+    const plan = {
+      name: "Arms Workout",
+      groups: [
+        {
+          exerciseID: exercise1._id,
+          sets: [
+            { type: "exercise", reps: 8, weight: 45 },
+            { type: "rest", duration: 60 },
+            {
+              type: "exercise",
+              reps: 8,
+              weight: 45,
+            },
+          ],
+        },
+        {
+          exerciseID: exercise2._id,
+          sets: [{ type: "exercise", reps: 10, weight: 55 }],
+        },
+      ],
+    };
+
+    await request(app)
+      .post("/api/1.0/plans")
+      .send(plan)
+      .set("x-auth-token", userToken1);
+
+    const savedPlan = await Plan.findOne({ name: "Arms Workout" });
+
+    await request(app)
+      .put(`/api/1.0/plans/${savedPlan._id}`)
+      .send({
+        name: "Arms Workout",
+        groups: [
+          {
+            exerciseID: exercise1._id,
+            sets: [
+              { type: "exercise", reps: 8, weight: 45 },
+              { type: "rest", duration: 60 },
+              {
+                type: "exercise",
+                reps: 8,
+                weight: 45,
+              },
+            ],
+          },
+        ],
+      })
+      .set("x-auth-token", userToken1);
+
+    const newPlan = await Plan.findOne({ name: "Arms Workout" });
+
+    expect(newPlan.groups.length).toBe(1);
+  });
 });

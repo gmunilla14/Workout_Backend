6e4b9edbc6c45a8a73aec1041e2d8bf064d7bd6a
gmunilla14 <gregmunilla@gmail.com>
2022-04-15 21:00:23 -0400

    Get port from heroku for build

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const app = require("./app");
 const mongoose = require("mongoose");
 
-app.listen(5000, () => console.log("App is listening..."));
+app.listen(process.env.PORT || 5000, () => console.log("App is listening..."));
 
 mongoose.connect(process.env.MONGO_URI).then(() => {
   console.log("MongoDB connection established....");

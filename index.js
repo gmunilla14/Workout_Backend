660e3f38b0768662e81b62c97f1df905e35bba04
gmunilla14 <gregmunilla@gmail.com>
2022-04-15 20:24:39 -0400

    Change port for build

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const app = require("./app");
 const mongoose = require("mongoose");
 
-app.listen(3000, "192.168.4.66", () => console.log("App is listening..."));
+app.listen(5000, () => console.log("App is listening..."));
 
 mongoose.connect(process.env.MONGO_URI).then(() => {
   console.log("MongoDB connection established....");
